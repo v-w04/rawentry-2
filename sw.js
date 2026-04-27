003d7fd61a931d7073f1e730d89c84df66b7ae97
v-w04 <victor.walmart.04@gmail.com>
2026-04-27 09:22:05 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+// Empty service worker — unregisters itself
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.registration.unregister())
+  );
+});
