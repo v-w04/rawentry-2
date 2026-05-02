fe31088fa48b24fd82fc1e2fae2c430e6bd4a902
v-w04 <victor.walmart.04@gmail.com>
2026-05-02 02:37:57 +0000

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Empty service worker — unregisters itself
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-    .then(() => self.registration.unregister())
-  );
-});
